0feaa85e7819d2c6b884eae466cb147f240769a5
Lamont V <lxveal1983@gmail.com>
2021-04-30 19:56:32 -0400

    Update App.js

--- perpetuity-frontend/src/App.js
+++ perpetuity-frontend/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './logo.jpg';
 import './App.css';
 
 function App() {
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+           Your Best Options
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Perpituity
         </a>
       </header>
     </div>
