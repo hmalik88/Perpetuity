9b7b4fb379c2ca54b87b0a818ee4de3322af2c35
hmalik88 <hbmalik88@gmail.com>
2021-05-06 02:07:07 -0400

    added ETH/BTC oracle client contracts, added in a strike sanity check at the auction/option creation points in the option factory contract, added in a constructor for the factory contract that takes in the oracle client addresses.

--- perpetuity-backend/contracts/OptionFactory.sol
+++ perpetuity-backend/contracts/OptionFactory.sol
@@ -3,6 +3,8 @@ pragma solidity ^0.7.3;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./Option.sol";
+import "./BTCConsumer.sol";
+import "./ETHConsumer.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC2O.sol";
 
 contract OptionFactory is Ownable {
@@ -21,24 +23,43 @@ contract OptionFactory is Ownable {
         address currentBidder;
     }
 
-    IERCO underlyingToken;
-    address maticWETH = 0xE8F3118fDB41edcFEF7bF1DCa8009Fa8274aa070;
-    address maticWBTC = 0x90ac599445B07c8aa0FC82248f51f6558136203D;
+    address maticWETH;
+    address maticWBTC;
+    BTCConsumer btcOracle;
+    ETHConsumer ethOracle;
+    address ETHoracle;
     Auction[] public auctions;
     address[] optionContracts;
 
+    constructor(address _BTCoracle, address _ETHoracle) public Ownable() {
+        maticWETH = 0xE8F3118fDB41edcFEF7bF1DCa8009Fa8274aa070;
+        maticWBTC = 0x90ac599445B07c8aa0FC82248f51f6558136203D;
+        btcOracle = BTCConsumer(_BTCoracle);
+        ethOracle = ETHConsumer(_ETHoracle);
+    }
+
+    modifier strikeSanityCheck(string _asset, bool _isCall, uint _strikePrice) {
+        require(_asset == "WETH" || _asset == "WBTC", "supported ERC-20 coins are only WETH and WBTC at the moment");
+        int256 price;
+        if (_asset == "WBTC") {
+            btcOracle.requestPriceData();
+            price = btcOracle.price();
+        } else {
+            ethOracle.requestPriceData();
+            price = ethOracle.price();
+        }
+        if (_isCall && _strikePrice > price) _;
+        else if (!_isCall && _strikePrice < price) _;
+    }
 
     function createAuction(string _asset,
                            uint _reservePrice,
                            uint _assetAmount,
                            uint _duration,
                            uint _strikePrice,
-                           bool _isCall) public {
-        require(_asset == "WETH" || _asset == "WBTC", "supported ERC-20 coins are only WETH and WBTC at the moment");
+                           bool _isCall) public strikeSanityCheck(_asset, _isCall, _strikePrice) {
         require(_reservePrice > 0, "reserve price must be a positive value");
         require(_duration >= 3, "duration of the auction must be atleast 3 days");
-        // insert logic to disallow creation of the call if the strike price is lower than the current asset price 
-        // insert logic to disallow creation of the put if the srike price is higher than the current asset price
         Auction memory newAuction = Auction({
             asset: _asset,
             assetAmount: _assetAmount,
@@ -73,12 +94,22 @@ contract OptionFactory is Ownable {
         //create CFA between option owner and bidder
         // emit an event that our contract is created and start the flow with the sdk on the front end?
         Auction memory auction = auctions[_auctionID];
+        int256 price;
         require(!auction.optionCreated, "this option was already written!");
         require(msg.sender == auction.owner, "You are not the owner!");
         require(block.timestamp > auction.creationTime + auction.duration * 1 days, "Auction is not yet over, please wait until after to create option");
         require(auction.currentBidder != address(0) && auction.currentBid > 0, "There are no bidders for the option!");
         require(auction.currentBid >= auction.reservePrice, "Reserve price was not met.");
         auction.optionCreated = true;
+        // need to check again if price makes sense with strike
+        if (auction.asset == "WBTC") {
+            btcOracle.requestPriceData();
+            price = btcOracle.price();
+        } else {
+            ethOracle.requestPriceData();
+            price = btcOracle.price();
+        }
+        require(auction.isCall ? price < auction.strikePrice : price > auction.strikePrice);
         address option = new Option(auction.asset,
                                     auction.assetAmount,
                                     auction.strikePrice,
@@ -86,10 +117,6 @@ contract OptionFactory is Ownable {
                                     auction.currentBid,
                                     auction.owner,
                                     auction.currentBidder);
-        if (auction.asset == "WETH") {
-
-        }
-        
         optionContracts.push(option);
     }
 
