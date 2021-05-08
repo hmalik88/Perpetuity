cd35b9f20bf9c4222f186b1a55adc0c7c7e47b4f
hmalik88 <hbmalik88@gmail.com>
2021-05-08 09:55:03 -0400

    added superfluid interfaces required to register factory as superapp, added superfluid callback and safety check methods/modifier, made chainlink consumer addresses private, changed createOption to a private method, changed its params to fit callback flow.

--- perpetuity-backend/contracts/OptionFactory.sol
+++ perpetuity-backend/contracts/OptionFactory.sol
@@ -5,10 +5,24 @@ import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
 import "./Option.sol";
 import "../node_modules/@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "../node_modules/@openzeppelin/contracts/utils/math/SafeMath.sol";
+import {
+    ISuperfluid,
+    ISuperToken,
+    ISuperAgreement,
+    SuperAppDefinitions
+} from "@superfluid-finance/ethereum-contracts/contracts/interfaces/superfluid/ISuperfluid.sol";
+
+import {
+    IConstantFlowAgreementV1
+} from "@superfluid-finance/ethereum-contracts/contracts/interfaces/agreements/IConstantFlowAgreementV1.sol";
+
+import {
+    SuperAppBase
+} from "@superfluid-finance/ethereum-contracts/contracts/apps/SuperAppBase.sol";
 import "./BTCConsumer.sol";
 import "./ETHConsumer.sol";
 
-contract OptionFactory is Ownable {
+contract OptionFactory is Ownable, SuperAppBase {
 
 using SafeMath for uint;
 
@@ -29,10 +43,15 @@ using SafeMath for uint;
     address maticWETH = 0xE8F3118fDB41edcFEF7bF1DCa8009Fa8274aa070;
     address maticWBTC = 0x90ac599445B07c8aa0FC82248f51f6558136203D;
     address maticDAI = 0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063;
-    address BTCoracle;
-    address ETHoracle;
+    address private BTCoracle;
+    address private ETHoracle;
     BTCConsumer btcOracle;
     ETHConsumer ethOracle;
+
+    ISuperfluid private host = ISuperfluid(0xEB796bdb90fFA0f28255275e16936D25d3418603);
+    address private fdaiX = 0x5D8B4C2554aeB7e86F387B4d6c00Ac33499Ed01f;
+    IConstantFlowAgreementV1 private cfa = IConstantFlowAgreementV1(0x49e565Ed1bdc17F3d220f72DF0857C26FA83F873);
+
     
     Auction[] public auctions;
     address[] optionContracts;
@@ -42,6 +61,13 @@ using SafeMath for uint;
         ETHoracle = _ETHoracle;
         btcOracle = BTCConsumer(BTCoracle);
         ethOracle = ETHConsumer(ETHoracle);
+        uint256 configWord =
+            SuperAppDefinitions.APP_LEVEL_FINAL |
+            SuperAppDefinitions.BEFORE_AGREEMENT_CREATED_NOOP |
+            SuperAppDefinitions.BEFORE_AGREEMENT_UPDATED_NOOP |
+            SuperAppDefinitions.BEFORE_AGREEMENT_TERMINATED_NOOP;
+
+        host.registerApp(configWord);
     }
 
     modifier strikeSanityCheck(string _asset, bool _isCall, uint _strikePrice) {
@@ -102,11 +128,14 @@ using SafeMath for uint;
         auction.currentBidder = msg.sender;
     }
 
-    function createOption(uint _auctionID) public {
+    function createOption(bytes calldata _ctx, bytes32 agreementId) private returns (bytes memory newCtx) {
         // we want to be able to have the CFA start when the writer creates the option.
         // reqire them to have a balance
         //create CFA between option owner and bidder
         // emit an event that our contract is created and start the flow with the sdk on the front end?
+        newCtx = _ctx;
+        address user = host.decodeCtx(_ctx).msgSender;
+        (,int96 flowRate,,) = cfa.getFlowByID(superToken, agreementId);
         Auction memory auction = auctions[_auctionID];
         int256 price;
         require(!auction.optionCreated, "this option was already written!");
@@ -174,5 +203,42 @@ using SafeMath for uint;
     function stringsEqual(string memory _a, string memory _b) internal returns (bool) {
         return (keccak256(abi.encodePacked((_a))) == keccak256(abi.encodePacked((_b))));
     }
+
+    function afterAgreementCreated(
+        ISuperToken _superToken,
+        address _agreementClass,
+        bytes32 _agreementId,
+        bytes calldata /*_agreementData*/,
+        bytes calldata _cbdata,
+        bytes calldata _ctx
+    )
+        external override
+        onlyExpected(_superToken, _agreementClass)
+        onlyHost
+        returns (bytes memory)
+    {
+        return createOption(_ctx, _agreementId);
+    }
+
+    function _isAccepted(ISuperToken _superToken) private view returns (bool) {
+        return address(_superToken) == address(fdaiX);
+    }
+
+    function _isCFAv1(address _agreementClass) private view returns (bool) {
+        return ISuperAgreement(_agreementClass).agreementType()
+            == keccak256("org.superfluid-finance.agreements.ConstantFlowAgreement.v1");
+    }
+
+    modifier onlyHost() {
+        require(msg.sender == address(host), "RedirectAll: support only one host");
+        _;
+    }
+
+    modifier onlyExpected(ISuperToken _superToken, address _agreementClass) {
+        require(_isAccepted(_superToken) , "Option: not accepted token");
+        require(_isCFAv1(_agreementClass), "Option: only CFAv1 supported");
+        _;
+    }
+
 
 }
